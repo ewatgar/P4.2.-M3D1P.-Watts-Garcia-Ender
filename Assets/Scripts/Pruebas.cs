cb7ab22a096e763f6cf9c3e70d0eb7bc63ff6105
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-01-16 12:42:06 +0100

    apuntes clase

--- Assets/Scripts/Pruebas.cs
+++ Assets/Scripts/Pruebas.cs
@@ -11,10 +11,27 @@ public class Pruebas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawLine(new Vector3(0,0,0), new Vector3(3,0,0), Color.yellow);
-        Debug.DrawRay(new Vector3(0,0,0), new Vector3(10,10,0), Color.red);
-        Debug.DrawRay(transform.position, transform.up, Color.red);
+        //Debug.DrawLine(new Vector3(0,0,0), new Vector3(3,0,0), Color.yellow);
+        //Debug.DrawRay(new Vector3(0,0,0), new Vector3(10,10,0), Color.red);
+        Debug.DrawRay(transform.position, transform.up, Color.green);
         Debug.DrawRay(transform.position, transform.forward, Color.blue);
-        Debug.DrawRay(transform.position, transform.right, Color.green);
+        Debug.DrawRay(transform.position, transform.right, Color.red);
+
+        /*PRODUCTO ESCALAR
+        Vector3 miPosicion = transform.position;
+        Vector3 posEnemigo = new Vector3(2,5);
+        Vector3 haciaEnemigo = (posEnemigo - miPosicion).normalized;
+
+        float prodEscalar = Vector3.Dot(transform.forward, haciaEnemigo);
+        Debug.Log(prodEscalar);*/
+
+        /*PRODUCTO VECTORIAL WIP
+        Vector3 a = Vector3.right, b =*/
+
+        
+
+        
+        
+        
     }
 }
